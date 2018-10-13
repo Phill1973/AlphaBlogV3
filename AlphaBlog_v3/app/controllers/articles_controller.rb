cb613b9b2877e0021ee4fb0ec8aa2e0bebce442f
philipp gleich <philipp_gleich@hotmail.de>
2018-10-13 06:17:43 -0400

    Show Action

--- AlphaBlog_v3/app/controllers/articles_controller.rb
+++ AlphaBlog_v3/app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
-  end  
+  end
+  
+  def index
+    @articles = Article.all
+  end
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to_articles_show(@article)
+    if @article.save
+      flash[:notice] = "Der Artikel wurde erfolgreich erstellt"
+      redirect_to articles_path(@article)      
+    else
+      render 'new'
+    end
+    
+    def show
+      @article = Article.find(params[:id])
+    end
+    
   end
   
   private
